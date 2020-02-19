0bf98cd40312b8364c285056909771613ecf4bb3
Christopher Leja <christopher.leja@gmail.com>
2020-02-19 21:36:40 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,23 @@
-
+class Book
+  
+  def initialize(title)
+    @title = title
+  end
+  
+  def title
+  end
+  
+  def author(author_name)
+    @author = author_name
+  end
+  
+  def number_of_pages(page_count)
+    @page_count = page_count
+  end
+  
+  def genre(genre)
+    @genre = genre
+  end
+  
+end
 
